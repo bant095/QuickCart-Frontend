10fa018b1c9d0fa00934a3468b93b1c04144422e
Abolaji Ayodeji <abolajiayodeji93@gmail.com>
2024-03-03 05:44:35 +0100

    I created Compare Product page

---
+++ src/pages/CompareProduct.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Meta from '../components/Meta';
+import BreadCrumb from '../components/BreadCrumb';
+
+const CompareProduct = () => {
+  return (
+    <>
+      <Meta title={'Compare Products'} />
+      <BreadCrumb title='Compare Products' />
+    </>
+  );
+};
+
+export default CompareProduct;
