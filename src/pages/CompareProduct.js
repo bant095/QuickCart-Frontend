a28035f2d2be732c7b529f3e2d903ba78b9f25ff
Abolaji Ayodeji <abolajiayodeji93@gmail.com>
2024-03-04 11:24:28 +0100

    I upadted the compare product page

--- src/pages/CompareProduct.js
+++ src/pages/CompareProduct.js
@@ -1,12 +1,107 @@
 import React from 'react';
 import Meta from '../components/Meta';
 import BreadCrumb from '../components/BreadCrumb';
+import Color from '../components/Color';
 
 const CompareProduct = () => {
   return (
     <>
       <Meta title={'Compare Products'} />
       <BreadCrumb title='Compare Products' />
+      <div className='compare-product-wrapper py-5 home-wrapper-2'>
+        <div className='row'>
+          <div className='col-3'>
+            <div className='compare-product-card position-relative'>
+              <img
+                src='images/cross.svg'
+                alt='cross'
+                className='position-absolute cross img-fluid'
+              />
+              <div className='product-card-image'>
+                <img src='images/watch.jpg' alt='watch' />
+              </div>
+
+              <div className='compare-product-details'>
+                <h5 className='title'>
+                  Honor T1 7.0 1GB RAM 8GB ROM 7 inches with Wi-fi+3G Tablet
+                </h5>
+                <h6 className='price mt-3'>$ 100</h6>
+              </div>
+
+              <div>
+                <div className='product-detail'>
+                  <h5>Brand:</h5>
+                  <p>Havels</p>
+                </div>
+                <div className='product-detail'>
+                  <h5>Type:</h5>
+                  <p>Watch</p>
+                </div>
+                <div className='product-detail'>
+                  <h5>Avaliability:</h5>
+                  <p>In Stock</p>
+                </div>
+                <div className='product-detail'>
+                  <h5>Color:</h5>
+                  <Color />
+                </div>
+                <div className='product-detail'>
+                  <h5>Size:</h5>
+                  <div className='d-flex gap-10'>
+                    <p>S</p>
+                    <p>M</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='col-3'>
+            <div className='compare-product-card position-relative'>
+              <img
+                src='images/cross.svg'
+                alt='cross'
+                className='position-absolute cross img-fluid'
+              />
+              <div className='product-card-image'>
+                <img src='images/watch.jpg' alt='watch' />
+              </div>
+
+              <div className='compare-product-details'>
+                <h5 className='title'>
+                  Honor T1 7.0 1GB RAM 8GB ROM 7 inches with Wi-fi+3G Tablet
+                </h5>
+                <h6 className='price mt-3'>$ 100</h6>
+              </div>
+
+              <div>
+                <div className='product-detail'>
+                  <h5>Brand:</h5>
+                  <p>Havels</p>
+                </div>
+                <div className='product-detail'>
+                  <h5>Type:</h5>
+                  <p>Watch</p>
+                </div>
+                <div className='product-detail'>
+                  <h5>Avaliability:</h5>
+                  <p>In Stock</p>
+                </div>
+                <div className='product-detail'>
+                  <h5>Color:</h5>
+                  <Color />
+                </div>
+                <div className='product-detail'>
+                  <h5>Size:</h5>
+                  <div className='d-flex gap-10'>
+                    <p>S</p>
+                    <p>M</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
