1cddf3a92aa8924f3f5b1fe6c55ca6cfe956c708
AYODEJI ABOLAJI ALFRED <abolajiayodeji93@gmail.com>
2024-03-13 12:30:47 +0100

    I added auth, pakages and set my functionality"

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -21,7 +21,16 @@ const ProductCard = (props) => {
           location.pathname === '/product' ? `gr-${grid}` : 'col-3'
         }`}
       >
-        <Link to=':id' className='product-card position-relative'>
+        <Link
+          to={`${
+            location.pathname === '/'
+              ? 'product/:id'
+              : location.pathname === '/product/:id'
+              ? '/product/:id'
+              : ':id'
+          }`}
+          className='product-card position-relative'
+        >
           <div className='wishlist-icon position-absolute'>
             <button className='border-0 bg-transparent'>
               <img src={wish} alt='wish' />
@@ -74,7 +83,16 @@ const ProductCard = (props) => {
           location.pathname === '/product' ? `col-${grid}` : 'col-3'
         }`}
       >
-        <Link className='product-card position-relative'>
+        <Link
+          to={`${
+            location.pathname === '/'
+              ? 'product/:id'
+              : location.pathname === '/product/:id'
+              ? '/product/:id'
+              : ':id'
+          }`}
+          className='product-card position-relative'
+        >
           <div className='wishlist-icon position-absolute'>
             <button className='border-0 bg-transparent'>
               <img src={wish} alt='wish' />
